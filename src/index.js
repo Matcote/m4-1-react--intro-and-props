7f3bd36fc05b058215e0d40de3df6356c97f768e
Matcote <matthewcotesipod@gmail.com>
2020-07-20 16:41:56 -0400

    ex 7

--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ import "./styles.css";
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-  <App currentUser={data.currentUser} conversation={data.conversation} />,
+  <div className="bigWrapper">
+    <App
+      currentUser={data.conversation.participants[1]}
+      conversation={data.conversation}
+    />
+    <App currentUser={data.currentUser} conversation={data.conversation} />
+  </div>,
   rootElement
 );
